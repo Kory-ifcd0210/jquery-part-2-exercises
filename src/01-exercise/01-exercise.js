65eed8848b27cdfaad0f5da9b08d26703f395af3
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-12-30 16:33:20 +0000

    1-2 ok

--- src/01-exercise/01-exercise.js
+++ src/01-exercise/01-exercise.js
@@ -2,6 +2,8 @@ import $ from "jquery";
 
 function exercise01() {
   // Complete the code of the function
+  var content=$(".box").css("padding");
+        $(".ex-wrapper").append("<p>"+content+"</p>");
 }
 
 export default exercise01;
--- src/03-exercise/03-exercise.js
+++ src/03-exercise/03-exercise.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 
 function exercise03() {
   // Complete the code of the function
+  
 }
 
 export default exercise03;
